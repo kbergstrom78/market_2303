8874e8d777ef1b868091d881e0c13902dc502368
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-04-17 11:29:10 -0600

    Add #overstocked_items and #sorted_items

--- lib/market.rb
+++ lib/market.rb
@@ -37,4 +37,23 @@ class Market
     end
     market_total
   end
+
+  def overstocked_items
+    o_items = []
+    total_inventory.each do |item, info|
+      o_items << item if info[:quantity] > 50 && info[:vendors].count > 1
+    end
+    o_items
+  end
+  # An array of Item objects that are overstocked. 
+  # An item is overstocked if it is sold by more than 1 vendor 
+  # AND the total quantity is greater than 50.
+
+  def sorted_item_list
+    total_inventory.keys.map do |item|
+      item.name
+    end.sort
+  end
+	# An array of the names of all items the Vendors have in stock, 
+  # sorted alphabetically. This list should not include any duplicate items.
 end
